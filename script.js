2391ca5f6486bc5e6a9979e45f208642758dec76
manchala3028 <manchala3028@gmail.com>
2020-09-02 10:34:24 +0530

    just left with ppt

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //select elemnet function
+/* menu part-1
 const selectElement = function (element){
     return document.querySelector(element);
 };
@@ -9,4 +10,19 @@ let body = selectElement('body');
 menuToggler.addEventListener('click',function(){
     body.classList.toggle('open');
 
+});*/
+
+/* menu part-2 */
+
+window.onload = () => {
+    setTimeout(() =>{
+    document.querySelector('body').classList.add
+    ('display');
+}, 500);
+};
+
+document.querySelector('.hamburger-menu').addEventListener('click',() => {
+    document.querySelector('body').classList.toggle('change');
 });
+
+
